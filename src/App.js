9202da9b9877facda20f63aa78f193a6600df72f
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2022-06-24 03:08:15 +0300

    Add useState examples

--- src/App.js
+++ src/App.js
@@ -1,22 +1,65 @@
-import { useState } from "react";
-import "./App.css";
-import React from "react";
+import React, { useState } from "react";
+import { Button, TextField } from "@mui/material";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const [userName, setUserName] = useState("ılkDeger");
+  const [password, setPassword] = useState("");
+  const [isUserLoggedIn, setIsUserLoggedIn] = useState(false);
+  const [error, setError] = useState(false);
 
-  const increment = () => {
-    setCount(count + 1);
+  const handleUserNameChange = (e) => {
+    setUserName(e.target.value);
   };
-  const decerement = () => {
-    setCount(count - 1);
+
+  const handlePasswordChange = (e) => {
+    setPassword(e.target.value);
   };
+
+  const handleLoginClick = () => {
+    if (userName && password) {
+      console.log(`Welcome, ${userName} ${password}`);
+      setIsUserLoggedIn(true);
+    } else {
+      setError(true);
+    }
+  };
+
+  // render
   return (
-    <div>
-      <h1>Welcome to my counter app</h1>
-      <p>The count is: {count}</p>
-      <button onClick={decerement}>-</button>
-      <button onClick={increment}>+</button>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+      }}
+    >
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "center",
+          flex: 1,
+          marginTop: 48,
+        }}
+      >
+        <TextField
+          id="outlined-basic"
+          label="User Name"
+          variant="outlined"
+          onChange={handleUserNameChange}
+        />
+        <TextField
+          id="outlined-basic"
+          label="Password"
+          variant="outlined"
+          onChange={handlePasswordChange}
+        />
+        <Button onClick={handleLoginClick} variant="contained">
+          Login
+        </Button>
+        {isUserLoggedIn && "You are logged ın"}
+      </div>
+      {isUserLoggedIn && `Welcome, ${userName}`}
     </div>
   );
 }
