2ac748f6fe493ea6c47524567c391bfceab5db71
Yavuz Mollahamzaoğlu <yavuz.mersin@hotmail.com>
2022-06-27 16:59:39 +0300

    Add basic list

--- src/App.js
+++ src/App.js
@@ -1,22 +1,18 @@
 import React, { useState, useEffect } from "react";
 
+
 function App() {
-  const [message, setMessage] = useState("initial")
-  const [num, setNum] = useState(1);
-  useEffect(function(){
-    console.log("Effect")
-  },[message, num])
+
 
   return(
     <div>
-      <h1>Effect</h1>
-      {message}
-      <button onClick = {() => {
-        setMessage("Hi!")
-      } } >Change</button>
-      {num}
-      <button onClick={() => {setNum(5)}}>num</button>
-
+      <h1> List of countries</h1>
+      <ul>
+        <li>Australia</li>
+        <li>Turkey</li>
+        <li>Poland</li>
+      </ul>
+    
     </div>
   )
 }
